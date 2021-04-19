86d1dce504b47fe01cb5e4fb3a9ed6ec279b718b
serkan <s.safran84@gmail.com>
2021-04-19 09:40:59 +0200

    adding files

---
+++ server/server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+const PORT = 9000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}) );
+
+app.all("/*", function(req, res, next){
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    next();
+});
+
+
+let allFriends = [{firstName: 'Coach', lastName: 'Tim', email: 'tim.broos@becode.org', phone: '0469420666', signatureMove: 'Yeet', languages: 'Javascript'}];
+
+// Below you can define how your API handles a get or a post request.
+// Try sending a get request to the root, you should get a "Hello from server" back.
+
+// app.get('/', function (request, response) {
+//     response.send('Hello from server');
+// });
+//
+// app.post('/', function (request, response) {
+//     response.status(200).send({"message": "Data received"});
+// });
+
+
+app.get("/allFriends", function (req,res){
+    res.send(allFriends);
+});
+
+app.post("/addFriends",function(req,res){
+    allFriends.push(req.body);
+})
+
+app.delete("/deleteFriends",function(req,res){
+    // allFriends.splice(index,req.body.email);
+    delete allFriends[req.body]
+})
+
+app.listen(PORT, function () {
+    console.log("Server is started on port 9000")
+});
